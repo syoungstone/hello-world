4309cd5143d6b6f80f05bb0ea2176372b8beeab5
syoungstone <53226974+syoungstone@users.noreply.github.com>
2019-08-21 14:10:33 -0400

    Added Hello.java
    Uploaded first java program

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*************************************************
+ * Java Program Name
+ *************************************************
+ * Project/Program description
+ * _______________________________________________
+ * Student First Name Last Name
+ * Date
+ * Course Number and Section
+ *************************************************/
+ 
+ public class Hello {
+	public static void printHeading() {
+		System.out.println("Your Name");
+		System.out.println("Java Project Name");
+		System.out.println("Project Description");
+		System.out.println("Version Date");
+		System.out.println("Course Number and Section");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+ }
